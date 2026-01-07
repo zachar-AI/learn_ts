4f1f3b6b2846fe27593a3dffe514ab4e21a29600
zachar-AI <zachskihill@gmail.com>
2026-01-06 19:57:45 -0500

    arrays

---
+++ 02Basics/myArray.js
@@ -0,0 +1,11 @@
+var superHeros = [];
+// const heroPower: number[] = []
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255],
+    []
+];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "", isActive: true });
---
+++ 02Basics/myArray.ts
@@ -0,0 +1,20 @@
+const superHeros: string[] = []
+// const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type User = {
+  name: string
+  isActive: boolean
+}
+
+const allUsers: Array<User> = []
+
+const MLModels: number[][] = [
+  [255, 255, 255],
+  []
+]
+
+superHeros.push("spiderman")
+heroPower.push(2)
+
+allUsers.push({name: "", isActive: true})
